b92058170d1f0132e291fb51748edbd7b6715351
Eduardo Bizarro <edbizarro@gmail.com>
2017-02-23 18:17:33 -0300

    imap

--- php/extensions.sh
+++ php/extensions.sh
@@ -16,6 +16,7 @@ apt-get update && apt-get install -y \
     && docker-php-ext-configure imap --with-kerberos --with-imap-ssl \
     && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
     && docker-php-ext-install -j$(nproc) \
+        imap \
         gd \
         curl \
         mcrypt \
