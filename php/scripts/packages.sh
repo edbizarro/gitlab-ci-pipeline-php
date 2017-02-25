fb6d56729e60008edbae5ec8d766a4f33f2538fa
Eduardo Bizarro <edbizarro@gmail.com>
2017-02-24 21:14:52 -0300

    scripts

--- php/scripts/packages.sh
+++ php/scripts/packages.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-export RUNTIME_PACKAGES="ca-certificates libmysqlclient-dev openssh-client unzip git curl nodejs libmcrypt-dev libfreetype6-dev libjpeg62-turbo-dev libpng12-dev libssl-dev libicu-dev libbz2-dev libc-client-dev libkrb5-dev zlib1g-dev"
-
 apt-get update \
 && DEBIAN_FRONTEND=noninteractive apt-get install -yqq \
-      $RUNTIME_PACKAGES \
+      libmysqlclient-dev \
+      openssh-client \
+      unzip \
+      git \
+      curl \
+      libmcrypt-dev \
+      libfreetype6-dev \
+      libjpeg62-turbo-dev \
+      libpng12-dev \
+      libssl-dev \
+      libicu-dev \
+      libbz2-dev \
+      libc-client-dev \
+      libkrb5-dev \
+      zlib1g-dev \
       apt-utils \
       --no-install-recommends
