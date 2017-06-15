e2f01427128ec198c613de98b29377320c5d668a
Eduardo Bizarro <edbizarro@gmail.com>
2017-06-15 09:00:49 -0300

    update NPM after node install

--- php/scripts/node.sh
+++ php/scripts/node.sh
@@ -3,5 +3,6 @@
 # NODE JS
 curl -sL https://deb.nodesource.com/setup_8.x | bash - \
     && DEBIAN_FRONTEND=noninteractive apt-get install nodejs --no-install-recommends -yqq \
+    && npm install -g npm \
     && npm install -g gulp \
     && curl -o- -L https://yarnpkg.com/install.sh | bash
