101d65c15cec34178abf32cd13cc5062ed56aab1
Eduardo Bizarro <edbizarro@gmail.com>
2017-06-15 09:00:36 -0300

    soap extension

--- php/scripts/extensions.sh
+++ php/scripts/extensions.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -yqq \
-    libmcrypt-dev \
-    libfreetype6-dev \
-    libjpeg62-turbo-dev \
-    libpng12-dev \
-    libgmp-dev \
-    libssl-dev \
-    libicu-dev \
     libbz2-dev \
+    libcurl4-gnutls-dev \
+    libfreetype6-dev \
+    libgmp-dev \
+    libicu-dev \
+    libjpeg62-turbo-dev \
+    libmcrypt-dev \
+    libpng12-dev \
+    libssl-dev \
+    libxml2-dev \
+    libzip-dev \
     && ln -s /usr/include/x86_64-linux-gnu/gmp.h /usr/include/gmp.h \
     && docker-php-ext-configure imap --with-kerberos --with-imap-ssl \
     && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
     && docker-php-ext-install -j$(nproc) \
         bcmath \
         bz2 \
+        curl \
         gd \
         gmp \
         imap \
         intl \
+        json \
         mbstring \
         mcrypt \
         opcache \
         pdo_mysql \
+        soap \
+        xml \
+        xmlrpc \
         zip
 
 # MONGO, xdebug and other extensions
-pecl install redis mongodb xdebug-2.5.1 apcu \
+pecl install redis mongodb xdebug-2.5.4 apcu \
   && docker-php-ext-enable redis mongodb xdebug apcu
