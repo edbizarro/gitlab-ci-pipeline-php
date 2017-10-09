8b732c28a2b7dd1812eef71d823fe9e0c5f3a721
Eduardo Bizarro <edbizarro@gmail.com>
2017-10-08 21:32:11 -0300

    first try on image tests with goss

--- php/scripts/alpine/extensions.sh
+++ php/scripts/alpine/extensions.sh
@@ -18,7 +18,7 @@ apk --update --no-cache add \
   gmp-dev \
   postgresql-dev \
   sqlite3
-
+  
 docker-php-ext-configure imap --with-kerberos --with-imap-ssl \
 && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
 && docker-php-ext-install -j$(getconf _NPROCESSORS_ONLN) \
