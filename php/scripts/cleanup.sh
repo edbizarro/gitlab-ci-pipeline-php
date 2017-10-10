b1d8da23f48b6464a0a2b43b366e190d5b2a869f
Eduardo Bizarro <edbizarro@gmail.com>
2017-10-10 19:17:30 -0300

    debian cleanup

--- php/scripts/cleanup.sh
+++ php/scripts/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get remove --purge -yqq $BUILD_PACKAGES apt-utils libc-client-dev \
+apt-get remove --purge -yqq $BUILD_PACKAGES apt-utils libc-client-dev make autoconf build-essential \
     && apt-get autoremove -y \
     && apt-get autoclean -y && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
