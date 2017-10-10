6606fd2ff4658e9e149f0547cf65c58126d2c3e1
Eduardo Bizarro <edbizarro@gmail.com>
2017-10-10 18:34:51 -0300

    debian tests

--- php/scripts/extensions.sh
+++ php/scripts/extensions.sh
@@ -20,6 +20,7 @@ apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -yqq \
     && docker-php-ext-install -j$(nproc) \
         bcmath \
         bz2 \
+        calendar \
         curl \
         exif \
         gd \
@@ -37,11 +38,35 @@ apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -yqq \
         soap \
         xml \
         xmlrpc \
+        xsl \
         zip
 
 # MONGO, xdebug and other extensions
 pecl channel-update pecl.php.net \
-  && pecl install redis mongodb xdebug-2.5.5 apcu memcached \
-  && docker-php-ext-enable redis mongodb xdebug apcu memcached
+  && pecl install redis mongodb xdebug-2.5.5 apcu memcached imagick \
+  && docker-php-ext-enable redis mongodb xdebug apcu memcached imagick
+
+docker-php-source delete
+
+{ \
+		echo 'opcache.memory_consumption=128'; \
+		echo 'opcache.interned_strings_buffer=8'; \
+		echo 'opcache.max_accelerated_files=4000'; \
+		echo 'opcache.revalidate_freq=60'; \
+		echo 'opcache.fast_shutdown=1'; \
+		echo 'opcache.enable_cli=1'; \
+} > /usr/local/etc/php/conf.d/opcache-recommended.ini
+
+{ \
+        echo 'apc.shm_segments=1'; \
+        echo 'apc.shm_size=256M'; \
+        echo 'apc.num_files_hint=7000'; \
+        echo 'apc.user_entries_hint=4096'; \
+        echo 'apc.ttl=7200'; \
+        echo 'apc.user_ttl=7200'; \
+        echo 'apc.gc_ttl=3600'; \
+        echo 'apc.max_file_size=1M'; \
+        echo 'apc.stat=1'; \
+} > /usr/local/etc/php/conf.d/apcu-recommended.ini
 
 echo "memory_limit=512M" > /usr/local/etc/php/conf.d/zz-conf.ini
