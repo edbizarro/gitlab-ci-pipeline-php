2aee2852b32bd110e933376b59b621ca64f1bcca
Eduardo Bizarro <edbizarro@gmail.com>
2018-01-25 17:12:38 -0200

    fix alpine

--- php/scripts/alpine/extensions.sh
+++ php/scripts/alpine/extensions.sh
@@ -35,7 +35,7 @@ docker-php-ext-configure imap --with-kerberos --with-imap-ssl
 docker-php-ext-install -j$(getconf _NPROCESSORS_ONLN) imap
 docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
 docker-php-ext-install -j$(getconf _NPROCESSORS_ONLN) gd
-docker-php-ext-install -j$(nproc) bcmath bz2 calendar iconv intl mbstring mysqli opcache pdo_mysql pdo_pgsql pgsql soap zip
+docker-php-ext-install -j$(getconf _NPROCESSORS_ONLN) bcmath bz2 calendar iconv intl mbstring mysqli opcache pdo_mysql pdo_pgsql pgsql soap zip
 docker-php-source delete
 
 if [[ $PHP_VERSION =~ "7.1" ]]; then
