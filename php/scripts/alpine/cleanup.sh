628bbdb974b9809b423c8b95d1f3ca0482f0239e
Eduardo Bizarro <edbizarro@gmail.com>
2018-01-26 01:12:44 -0200

    build test

--- php/scripts/alpine/cleanup.sh
+++ php/scripts/alpine/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apk del --purge grep build-base .build-deps
+#apk del --purge grep build-base .build-deps
 
 rm -rf /var/cache/apk/* /tmp/* /usr/share/man /usr/include/php7 /usr/lib/php7/build /usr/lib/php7/modules/*.a
