70f017d2cfb387047f7ba0ed9e068b76c9514c93
Eduardo Bizarro <edbizarro@gmail.com>
2018-01-26 15:16:00 -0200

    fix envs

--- php/scripts/alpine/packages.sh
+++ php/scripts/alpine/packages.sh
@@ -15,8 +15,10 @@ apk add --update --no-cache \
 
 if [[ $PHP_VERSION =~ "7.2" ]]
 then
+  echo $PHP_VERSION
   apk add --update --no-cache libressl-dev
 else
+  echo $PHP_VERSION
   apk add --update --no-cache openssl-dev
 fi
 
