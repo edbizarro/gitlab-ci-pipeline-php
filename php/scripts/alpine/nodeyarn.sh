80b44366ee21659cc9b65595519d0b646f741b07
Eduardo Bizarro <edbizarro@gmail.com>
2018-03-14 16:59:09 -0300

    node version on alpine

--- php/scripts/alpine/nodeyarn.sh
+++ php/scripts/alpine/nodeyarn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e 'http://dl-cdn.alpinelinux.org/alpine/edge/main\nhttp://dl-cdn.alpinelinux.org/alpine/edge/community\nhttp://dl-cdn.alpinelinux.org/alpine/edge/testing' > /etc/apk/repositories && \
+echo 'http://dl-cdn.alpinelinux.org/alpine/edge/main\nhttp://dl-cdn.alpinelinux.org/alpine/edge/community\nhttp://dl-cdn.alpinelinux.org/alpine/edge/testing' > /etc/apk/repositories
 
 apk add --update nodejs nodejs-npm yarn
 
