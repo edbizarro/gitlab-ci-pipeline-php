ad2060c558ff2a288176337cc3aeed7e1a7cafcc
Eduardo Bizarro <edbizarro@gmail.com>
2018-04-21 22:45:26 -0300

    node alpine

--- php/scripts/alpine/nodeyarn.sh
+++ php/scripts/alpine/nodeyarn.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo 'http://dl-cdn.alpinelinux.org/alpine/edge/main\nhttp://dl-cdn.alpinelinux.org/alpine/edge/community\nhttp://dl-cdn.alpinelinux.org/alpine/edge/testing' > /etc/apk/repositories && \
 
-apk add --update nodejs nodejs-npm yarn
+apk add --update --no-cache nodejs-current-npm yarn
 
 npm install -g npm
