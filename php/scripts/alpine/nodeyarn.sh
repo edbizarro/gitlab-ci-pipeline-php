dcdcafc200d0de652003b89441053d5984503ebf
Eduardo Bizarro <edbizarro@gmail.com>
2018-05-04 20:57:52 -0300

    trying to install node via nvm

--- php/scripts/alpine/nodeyarn.sh
+++ php/scripts/alpine/nodeyarn.sh
@@ -5,6 +5,8 @@ set -euf -o pipefail
 export VERSION=v10.0.0
 
  # NVM
+apk add --no-cache -U curl bash ca-certificates openssl ncurses coreutils python make gcc g++ libgcc linux-headers grep util-linux binutils findutils
+
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 { \
@@ -15,10 +17,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 
 source ~/.profile
 
-ls /home/php/.nvm
-ls /home/php/.nvm/.cache
-
-nvm install node
+nvm install -s node
 nvm use --delete-prefix $VERSION
 nvm alias default node
 npm i -g npm
