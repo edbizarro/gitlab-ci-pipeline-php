f6d1218bbbf04ec1d3841979794b1d5b715aa919
Eduardo Bizarro <edbizarro@gmail.com>
2018-05-04 20:29:04 -0300

    trying to install node via nvm

--- php/scripts/alpine/nodeyarn.sh
+++ php/scripts/alpine/nodeyarn.sh
@@ -3,19 +3,20 @@
 set -euf -o pipefail
 
 export VERSION=v10.0.0
-export NPM_VERSION=6
-export YARN_VERSION=latest
 
  # NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 { \
-  echo 'export NVM_DIR="/home/www-data/.nvm"'; \
+  echo 'export NVM_DIR="$HOME/.nvm"'; \
   echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm'; \
 
 } > ~/.profile
 
 source ~/.profile
+
+ls /home/php/.nvm
+ls /home/php/.nvm/.cache
 
 nvm install node
 nvm use --delete-prefix $VERSION
