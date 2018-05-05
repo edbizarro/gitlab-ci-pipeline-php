b41ea626a85c2abcaea53b7fec858dfbb0595f77
Eduardo Bizarro <edbizarro@gmail.com>
2018-05-05 00:28:38 -0300

    change node script

--- php/scripts/alpine/nodeyarn.sh
+++ php/scripts/alpine/nodeyarn.sh
@@ -2,18 +2,18 @@
 
 set -euf -o pipefail
 
-export VERSION=v10.0.0
+export NODE_VERSION=v10.0.0
 
  # NVM
 apk add --no-cache -U  openssl ncurses coreutils make gcc g++ libgcc linux-headers grep util-linux binutils findutils
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-
-source ~/.nvm/nvm.sh
-
-nvm install -s node >/dev/null
-nvm use --delete-prefix $VERSION
-nvm alias default node
+cd /tmp \
+  && curl https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz > nodejs-src.tar.gz \
+  && tar xf nodejs-src.tar.gz \
+  && cd node-v${NODE_VERSION} \
+  && ./configure --prefix=/usr \
+  && make -j`getconf _NPROCESSORS_ONLN` \
+  && make install
 npm i -g npm
 
 curl -o- -L https://yarnpkg.com/install.sh | bash
