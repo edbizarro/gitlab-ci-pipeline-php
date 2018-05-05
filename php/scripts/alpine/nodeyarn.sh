b4a4a88df8b39ddfa19d7575b9520f4e3db00418
Eduardo Bizarro <edbizarro@gmail.com>
2018-05-04 21:35:34 -0300

    nvm

--- php/scripts/alpine/nodeyarn.sh
+++ php/scripts/alpine/nodeyarn.sh
@@ -9,15 +9,9 @@ apk add --no-cache -U curl bash ca-certificates openssl ncurses coreutils python
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-{ \
-  echo 'export NVM_DIR="$HOME/.nvm"'; \
-  echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm'; \
+source ~/.nvm/nvm.sh
 
-} > ~/.profile
-
-source ~/.profile
-
-nvm install node
+nvm install -s node
 nvm use --delete-prefix $VERSION
 nvm alias default node
 npm i -g npm
