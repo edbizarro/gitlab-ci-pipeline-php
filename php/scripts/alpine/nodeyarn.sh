0afa61729a15f72b2d93983ed7e41bed4a0457d9
Eduardo Bizarro <edbizarro@gmail.com>
2018-05-07 12:21:44 -0300

    ops

--- php/scripts/alpine/nodeyarn.sh
+++ php/scripts/alpine/nodeyarn.sh
@@ -2,12 +2,11 @@
 
 ln -s /usr/lib/npm/bin/npm-cli.js /usr/bin/npm
 
-npm install -g npm@${NPM_VERSION}
+npm i -g npm@${NPM_VERSION}
+npm i -g gulp
 
 curl -o- -L https://yarnpkg.com/install.sh | bash
 
 rm -rf /usr/share/man /var/cache/apk/* \
   /root/.npm /root/.node-gyp /root/.gnupg /usr/lib/node_modules/npm/man \
   /usr/lib/node_modules/npm/doc /usr/lib/node_modules/npm/html /usr/lib/node_modules/npm/scripts
-
-npm i -g gulp
