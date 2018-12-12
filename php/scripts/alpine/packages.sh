f803fc83a563d58eed2df7a1424c2676db5a9432
Eduardo Bizarro <edbizarro@gmail.com>
2018-12-12 09:31:29 -0200

    php 7.3

--- php/scripts/alpine/packages.sh
+++ php/scripts/alpine/packages.sh
@@ -16,9 +16,7 @@ apk add --no-cache \
     rsync \
     sudo
 
-if [[ $PHP_VERSION -eq "7.3" ]]; then
-  apk add --no-cache libressl-dev
-elif [[ $PHP_VERSION -eq "7.2" ]]; then
+if [[ $PHP_VERSION == "7.3" || $PHP_VERSION == "7.2" ]]; then
   apk add --no-cache libressl-dev
 else
   apk add --no-cache openssl-dev
