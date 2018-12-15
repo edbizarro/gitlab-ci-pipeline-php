c5757a896e7349493082ce00d86325e98d1c6b3d
Eduardo Bizarro <edbizarro@gmail.com>
2018-12-15 01:54:33 -0200

    opcache and apc configs

--- php/scripts/alpine/extensions.sh
+++ php/scripts/alpine/extensions.sh
@@ -106,23 +106,25 @@ git clone "https://github.com/php-memcached-dev/php-memcached.git" \
     && docker-php-ext-enable memcached
 
 { \
-		echo 'opcache.memory_consumption=128'; \
-		echo 'opcache.interned_strings_buffer=8'; \
-		echo 'opcache.max_accelerated_files=4000'; \
-		echo 'opcache.revalidate_freq=60'; \
-		echo 'opcache.fast_shutdown=1'; \
-		echo 'opcache.enable_cli=1'; \
+    echo 'opcache.enable=1'; \
+    echo 'opcache.revalidate_freq=0'; \
+    echo 'opcache.validate_timestamps=1'; \
+    echo 'opcache.max_accelerated_files=10000'; \
+    echo 'opcache.memory_consumption=192'; \
+    echo 'opcache.max_wasted_percentage=10'; \
+    echo 'opcache.interned_strings_buffer=16'; \
+    echo 'opcache.fast_shutdown=1'; \
 } > /usr/local/etc/php/conf.d/opcache-recommended.ini
 
 { \
     echo 'apc.shm_segments=1'; \
-    echo 'apc.shm_size=256M'; \
+    echo 'apc.shm_size=512M'; \
     echo 'apc.num_files_hint=7000'; \
     echo 'apc.user_entries_hint=4096'; \
     echo 'apc.ttl=7200'; \
     echo 'apc.user_ttl=7200'; \
     echo 'apc.gc_ttl=3600'; \
-    echo 'apc.max_file_size=1M'; \
+    echo 'apc.max_file_size=50M'; \
     echo 'apc.stat=1'; \
 } > /usr/local/etc/php/conf.d/apcu-recommended.ini
 
