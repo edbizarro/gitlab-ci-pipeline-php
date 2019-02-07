8a107f76ec74e1fa4e763a2660f3482d2692ff79
Eduardo Bizarro <edbizarro@gmail.com>
2019-02-07 02:15:35 -0200

    alpine scripts

--- php/scripts/alpine/packages.sh
+++ php/scripts/alpine/packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apk update && apk upgrade
+apk update && apk upgrade --no-cache
 
 apk add --no-cache \
     ca-certificates \
@@ -11,16 +11,11 @@ apk add --no-cache \
     git \
     grep \
     make \
-    openssh-client \
     python \
     rsync \
     sudo
 
-if [[ $PHP_VERSION == "7.1" ]]; then
-  apk add --no-cache openssl openssl-dev
-else
-  apk add --no-cache libressl-dev
-fi
+apk add --no-cache openssl openssl-dev
 
 apk add --no-cache --virtual .build-deps build-base autoconf
 
