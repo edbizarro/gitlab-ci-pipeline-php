21894764770d8f5068b80221032307c841a97532
Eduardo Bizarro <edbizarro@gmail.com>
2019-04-05 13:54:45 -0300

    👌 trying reduce size of debian images

--- php/scripts/node.sh
+++ php/scripts/node.sh
@@ -7,6 +7,7 @@ curl -sL https://deb.nodesource.com/setup_11.x | bash - \
     && DEBIAN_FRONTEND=noninteractive apt-get install nodejs --no-install-recommends -yqq \
     && npm i -g npm \
     && npm i -g gulp \
-    && curl -o- -L https://yarnpkg.com/install.sh | bash
+    && curl -o- -L https://yarnpkg.com/install.sh | bash \
+    && npm cache clean --force
 
 xargs sudo chmod a+x $HOME/.yarn/bin/yarn
