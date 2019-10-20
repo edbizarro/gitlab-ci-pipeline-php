eab364ad92f95618c8fb9a97593b15614117717f
Eduardo Bizarro <edbizarro@gmail.com>
2019-10-19 22:29:04 -0300

    Update cleanup.sh

--- php/scripts/cleanup.sh
+++ php/scripts/cleanup.sh
@@ -15,4 +15,4 @@ apt-get purge -yqq --auto-remove -o APT::AutoRemove::RecommendsImportant=false -
 apt-get autoremove -yqq --purge
 apt-get autoclean -yqq
 apt-get clean
-rm -rf /var/cache/apt/ /var/lib/apt/lists/* /var/log/* /tmp /var/tmp/* /usr/share/doc /usr/share/doc-base /usr/share/groff/* /usr/share/info/* /usr/share/linda/* /usr/share/lintian/overrides/* /usr/share/locale/* /usr/share/man/* /usr/share/locale/* /usr/share/gnome/help/*/* /usr/share/doc/kde/HTML/*/* /usr/share/omf/*/*-*.emf
+rm -rf /var/cache/apt/ /var/lib/apt/lists/* /var/log/* /tmp/* /var/tmp/* /usr/share/doc /usr/share/doc-base /usr/share/groff/* /usr/share/info/* /usr/share/linda/* /usr/share/lintian/overrides/* /usr/share/locale/* /usr/share/man/* /usr/share/locale/* /usr/share/gnome/help/*/* /usr/share/doc/kde/HTML/*/* /usr/share/omf/*/*-*.emf
