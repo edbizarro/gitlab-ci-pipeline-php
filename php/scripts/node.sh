b5aa9750acc7a4e582d31d83f58d1653bac38275
Eduardo Bizarro <edbizarro@gmail.com>
2019-10-28 09:57:57 -0300

    fix NPM version #73

--- php/scripts/node.sh
+++ php/scripts/node.sh
@@ -3,7 +3,7 @@
 set -euf -o pipefail
 
 # NODE JS
-curl -sL https://deb.nodesource.com/setup_12.x | bash - \
+curl -sL https://deb.nodesource.com/setup_13.x | bash - \
     && DEBIAN_FRONTEND=noninteractive apt-get install nodejs -yqq \
     && npm i -g npm \
     && curl -o- -L https://yarnpkg.com/install.sh | bash \
