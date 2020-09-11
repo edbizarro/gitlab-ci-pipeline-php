2e59517d704a3f7b6bfad0447b8c3a5b9907f2f5
Eduardo Bizarro <edbizarro@gmail.com>
2020-09-11 15:01:10 -0300

    remove some foldersfrom cleanup script

--- php/scripts/alpine/cleanup.sh
+++ php/scripts/alpine/cleanup.sh
@@ -7,6 +7,5 @@ apk del --purge grep build-base file openssl-dev
 rm -rf /tmp/* \
         /usr/includes/* \
         /usr/share/man/* \
-        /usr/src/* \
         /var/cache/apk/* \
         /var/tmp/*
