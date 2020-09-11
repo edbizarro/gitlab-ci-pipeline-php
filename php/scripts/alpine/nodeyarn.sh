1a456604e37c41cc2c07730f67b3c0cebf6bb630
Eduardo Bizarro <edbizarro@gmail.com>
2020-09-11 06:31:53 -0300

    npm madness https://github.com/npm/cli/issues/611

--- php/scripts/alpine/nodeyarn.sh
+++ php/scripts/alpine/nodeyarn.sh
@@ -8,8 +8,6 @@ npm i -g --force npm@${NPM_VERSION}
 
 curl -o- -L https://yarnpkg.com/install.sh | bash
 
-mkdir $HOME/.config
-
 rm -rf /usr/share/man /var/cache/apk/* \
   /root/.npm /root/.node-gyp /root/.gnupg /usr/lib/node_modules/npm/man \
   /usr/lib/node_modules/npm/doc /usr/lib/node_modules/npm/html /usr/lib/node_modules/npm/scripts
